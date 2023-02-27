c0a4eb041699ad008102bbd3510715edc560d92c
Pedrogabriel152 <105892538+Pedrogabriel152@users.noreply.github.com>
2023-02-27 11:10:09 -0300

    Criando a timeline

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -11,13 +11,44 @@ class AppController extends Action
     public function timeline() {
         session_start();
 
+        $this->validaAutenticacao();
+
+        // recuperando os tweets
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $this->view->tweets = $tweet->getAll();
+
+        $this->render('timeline');
+    }
+
+    public function tweet() {
+
+        session_start();
+
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweet->salvar();
+
+        header('Location: /timeline');
+
+    }
+
+    public function validaAutenticacao() {
+
         if(empty($_SESSION['id']) || empty($_SESSION['nome'])){
             session_destroy();
             header('Location: /?login=erro');
             exit();
         }
 
-        $this->render('timeline');
+        return true;
     }
 
 }
